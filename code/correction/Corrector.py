83940439e46545c805eb0c0c2db7736909eecebf
Amber on abhyasa <amber@abhyasa>
2013-03-25 13:16:50 -0600

    Implemented SentencePath class for corrector beam search.

---
+++ code/correction/Corrector.py
@@ -0,0 +1,33 @@
+# L. Amber Wilcox-O'Hearn 2013
+# Corrector.py
+
+class SentencePath:
+    """
+    This recursive structure represents a partial sentence.  It
+    consists of the SentencePath it was derived from (prior), the new
+    word that was added to that prior, and the new total log
+    probability associated with the partial sentence.
+    """
+    def __init__(self, word, prob, prior):
+        self.word = word
+        self.prob = prob
+        self.prior = prior
+    def __cmp__(self, other):
+        return cmp(self.prob, other.prob)
+    def tokens(self):
+        reverse_tokens = [self.word]
+        p = self.prior
+        while p:
+            reverse_tokens.append(p.word)
+            p = p.prior
+        reverse_tokens.reverse()
+        return reverse_tokens
+    def __repr__(self):
+        path_tuple_list = [(self.word, self.prob)]
+        p = self.prior
+        while p:
+            path_tuple_list.append((p.word, p.prob))
+            p = p.prior
+        path_tuple_list.reverse()
+        return '<%s %s>' % (self.__class__.__name__, repr(path_tuple_list))
+
---
+++ code/correction/test/test_Corrector.py
@@ -0,0 +1,25 @@
+# L. Amber Wilcox-O'Hearn 2013
+# test_Corrector.py
+
+from code.correction import Corrector
+import unittest 
+
+class SentencePathTest(unittest.TestCase):
+
+    def test_sentence_path(self):
+        path = Corrector.SentencePath('This', -.7, None)
+        new_path = Corrector.SentencePath('is', -.8, path)
+        newer_path = Corrector.SentencePath('fun', -1.1, new_path)
+        assert repr(path) == "<SentencePath [('This', -0.7)]>", path
+        assert repr(new_path) == "<SentencePath [('This', -0.7), ('is', -0.8)]>", new_path
+        assert repr(newer_path) == "<SentencePath [('This', -0.7), ('is', -0.8), ('fun', -1.1)]>", newer_path
+        self.assertListEqual(path.tokens(), ['This'], path.tokens())
+        self.assertListEqual(new_path.tokens(), ['This', 'is'], new_path.tokens())
+        self.assertListEqual(newer_path.tokens(), ['This', 'is', 'fun'], newer_path.tokens())
+        a_list = [new_path, newer_path, path]
+        a_list.sort()
+        self.assertListEqual(a_list, [newer_path, new_path, path], a_list)
+        
+
+if __name__ == '__main__':
+    unittest.main()
