c7ff6c96c14701fee8534e3b8b9e8fd9c1ba527b
Amber on abhyasa <amber@abhyasa>
2013-03-25 13:24:18 -0600

    Made assertions types more consistent.

--- code/correction/test/test_Corrector.py
+++ code/correction/test/test_Corrector.py
@@ -10,15 +10,21 @@ class SentencePathTest(unittest.TestCase):
         path = Corrector.SentencePath('This', -.7, None)
         new_path = Corrector.SentencePath('is', -.8, path)
         newer_path = Corrector.SentencePath('fun', -1.1, new_path)
-        assert repr(path) == "<SentencePath [('This', -0.7)]>", path
-        assert repr(new_path) == "<SentencePath [('This', -0.7), ('is', -0.8)]>", new_path
-        assert repr(newer_path) == "<SentencePath [('This', -0.7), ('is', -0.8), ('fun', -1.1)]>", newer_path
+
+        self.assertEqual(repr(path), "<SentencePath [('This', -0.7)]>", path)
+        self.assertEqual(repr(new_path), "<SentencePath [('This', -0.7), ('is', -0.8)]>", new_path)
+        self.assertEqual(repr(newer_path), "<SentencePath [('This', -0.7), ('is', -0.8), ('fun', -1.1)]>", newer_path)
         self.assertListEqual(path.tokens(), ['This'], path.tokens())
         self.assertListEqual(new_path.tokens(), ['This', 'is'], new_path.tokens())
         self.assertListEqual(newer_path.tokens(), ['This', 'is', 'fun'], newer_path.tokens())
         a_list = [new_path, newer_path, path]
         a_list.sort()
         self.assertListEqual(a_list, [newer_path, new_path, path], a_list)
+        
+class BeamTest(unittest.TestCase):
+
+    def test_beam(self):
+        self.skipTest("")
         
 
 if __name__ == '__main__':
