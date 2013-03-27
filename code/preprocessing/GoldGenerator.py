2882b68f44d7fb620249b5a692b5e9a918d50ebd
Amber on abhyasa <amber@abhyasa>
2013-03-27 06:47:46 -0600

    Finished GoldGeneratorand tests.  Realized I didn't need a class per se.  (Have been using them unnecessarily.)

--- code/preprocessing/GoldGenerator.py
+++ code/preprocessing/GoldGenerator.py
@@ -4,25 +4,42 @@
 # Takes the m2scorer formatted file and generates a file with one
 # corrected sentence per line.
 
-class GoldGenerator():
+def correct_sentence(sentence, corrections):
+    tokens = sentence.split()[1:]
+    span_substitions = []
+    for line in corrections:
+        correction_string_tokens = line.split('|||')
+        indices = [int(x) for x in correction_string_tokens[0].split()[1:]]
+        substitution_string = correction_string_tokens[2]
+        span_substitions.append((indices, substitution_string))
+    span_substitions.sort()
+    corrected = ""
+    index = 0
+    for sub in span_substitions:
+        corrected += " ".join(tokens[index:sub[0][0]]) + " " + sub[1] + " "
+        index = sub[0][1]
+    corrected += " ".join(tokens[index:])
+    return " ".join(corrected.split()) + '\n'
 
-    def __init__(self, m2_file_obj, gold_file_obj):
-        self.m2_file_obj = m2_file_obj
-        self.gold_file_obj = gold_file_obj
+def correct_file(m2_file_obj, gold_file_obj):
+    corrections = []
+    new_sentence = True
+    for line in m2_file_obj:
+        if line.startswith("S "):
+            assert new_sentence
+            sentence = line
+            new_sentence = False
+        elif line.startswith("A "):
+            assert not new_sentence
+            corrections.append(line)
+        else:
+            assert line == '\n', line
+            gold_file_obj.write(correct_sentence(sentence, corrections))
+            corrections = []
+            new_sentence = True
 
-    def correct_sentence(self, sentence, corrections):
-        tokens = sentence.split()[1:]
-        span_substitions = []
-        for line in corrections:
-            correction_string_tokens = line.split('|||')
-            indices = [int(x) for x in correction_string_tokens[0].split()[1:]]
-            substitution_string = correction_string_tokens[2]
-            span_substitions.append((indices, substitution_string))
-        span_substitions.sort()
-        corrected = ""
-        index = 0
-        for sub in span_substitions:
-            corrected += " ".join(tokens[index:sub[0][0]]) + " " + sub[1] + " "
-            index = sub[0][1]
-        corrected += " ".join(tokens[index:])
-        return " ".join(corrected.split())
+if __name__ == '__main__':
+    import sys
+    m2_file_obj = sys.stdin
+    gold_file_obj = sys.stdout
+    correct_file(m2_file_obj, gold_file_obj)
--- code/preprocessing/test/test_GoldGenerator.py
+++ code/preprocessing/test/test_GoldGenerator.py
@@ -8,29 +8,64 @@ class GoldTest(unittest.TestCase):
 
     def test_correct_sentence(self):
 
-        input_file_obj = StringIO.StringIO()
-        output_file_obj = StringIO.StringIO()
-        gg = GoldGenerator.GoldGenerator(input_file_obj, output_file_obj)
-
-        sentence = "S Our current population is 6 billion people and it is still growing exponentially ."
+        sentence = "S Our current population is 6 billion people and it is still growing exponentially .\n"
         corrections = []
-        corrected = gg.correct_sentence(sentence, corrections)
-        self.assertEqual(corrected, "Our current population is 6 billion people and it is still growing exponentially .", corrected)
+        corrected = GoldGenerator.correct_sentence(sentence, corrections)
+        self.assertEqual(corrected, "Our current population is 6 billion people and it is still growing exponentially .\n", corrected)
 
-        sentence = "S This will , if not already , caused problems as there are very limited spaces for us ."
+        sentence = "S This will , if not already , caused problems as there are very limited spaces for us .\n"
         corrections = ["A 7 8|||Vform|||cause|||REQUIRED|||-NONE-|||0", 
                        "A 14 15|||Nn|||space|||REQUIRED|||-NONE-|||0",
                        "A 11 12|||SVA|||is|||REQUIRED|||-NONE-|||0"]
-        corrected = gg.correct_sentence(sentence, corrections)
-        self.assertEqual(corrected, "This will , if not already , cause problems as there is very limited space for us .", corrected)
+        corrected = GoldGenerator.correct_sentence(sentence, corrections)
+        self.assertEqual(corrected, "This will , if not already , cause problems as there is very limited space for us .\n", corrected)
 
 
-        sentence = "S It is also important to create a better material that can support the buildings despite any natural disaster like earthquakes ."
+        sentence = "S It is also important to create a better material that can support the buildings despite any natural disaster like earthquakes .\n"
         corrections = ["A 6 7|||ArtOrDet||||||REQUIRED|||-NONE-|||0",
                        "A 12 13|||ArtOrDet||||||REQUIRED|||-NONE-|||0",
                        "A 17 18|||Nn|||disasters|||REQUIRED|||-NONE-|||0"]
-        corrected = gg.correct_sentence(sentence, corrections)
-        self.assertEqual(corrected, "It is also important to create better material that can support buildings despite any natural disasters like earthquakes .", corrected)
+        corrected = GoldGenerator.correct_sentence(sentence, corrections)
+        self.assertEqual(corrected, "It is also important to create better material that can support buildings despite any natural disasters like earthquakes .\n", corrected)
+
+    def test_correct_file(self):
+
+        input_file_obj = ["S However , when the design had been carried out for a few weeks , it was stopped .\n",
+             "\n",
+             "S The government 's explanation was because of safety consideration for earthquakes and some other disasters .\n",
+             "\n",
+             "S But such explanation was of non-sense as the engineers did not see such possibility during their designing ; otherwise the design would not be carried out at all .\n",
+             "A 4 5|||Prep||||||REQUIRED|||-NONE-|||0\n",
+             "A 2 3|||ArtOrDet|||an explanation|||REQUIRED|||-NONE-|||0\n",
+             "A 13 14|||ArtOrDet|||a possibility|||REQUIRED|||-NONE-|||0\n",
+             "\n",
+             "S Some evidences showed that the reason of halt of work was that the higher level of authorities thought an underground traffic system was not necessary for Harbin .\n",
+             "A 6 7|||Prep|||for|||REQUIRED|||-NONE-|||0\n",
+             "A 7 8|||ArtOrDet|||the halt|||REQUIRED|||-NONE-|||0\n",
+             "A 9 10|||ArtOrDet|||the work|||REQUIRED|||-NONE-|||0\n",
+             "A 15 16|||ArtOrDet||||||REQUIRED|||-NONE-|||0\n",
+             "\n",
+             "S In this example , there is nothing wrong with the original design work , but the design still can not be carried out due to political inconsistent between the city government and higher level authorities .\n",
+             "\n",
+             "S The similarities of political obstacles in both two steps are that such problems can not be easily solved by the engineering group , in other words , the political problems are unavoidable to some degree .\n",
+             "\n",
+             "S Another problem , financial issue , is quite common during design process .\n",
+             "A 10 11|||ArtOrDet|||the design|||REQUIRED|||-NONE-|||0\n",
+             "\n",
+             "S It affects the analyzing and test step most seriously though it can affect almost every step of design work .\n",
+             "A 5 6|||ArtOrDet|||the testing|||REQUIRED|||-NONE-|||0\n",
+             "\n",
+             "S For most design work , the engineers are always employed by some organizations or companies .\n",
+             "A 5 6|||ArtOrDet||||||REQUIRED|||-NONE-|||0\n",
+             "\n",
+             "S Without financial support , the collection of relative information and research of the various solutions have to be done in a simpler manner , and may lead to the final failure of a design project .\n"]
+
+        expected_output = "However , when the design had been carried out for a few weeks , it was stopped .\nThe government 's explanation was because of safety consideration for earthquakes and some other disasters .\nBut such an explanation was non-sense as the engineers did not see such a possibility during their designing ; otherwise the design would not be carried out at all .\nSome evidences showed that the reason for the halt of the work was that the higher level authorities thought an underground traffic system was not necessary for Harbin .\nIn this example , there is nothing wrong with the original design work , but the design still can not be carried out due to political inconsistent between the city government and higher level authorities .\nThe similarities of political obstacles in both two steps are that such problems can not be easily solved by the engineering group , in other words , the political problems are unavoidable to some degree .\nAnother problem , financial issue , is quite common during the design process .\nIt affects the analyzing and the testing step most seriously though it can affect almost every step of design work .\nFor most design work , engineers are always employed by some organizations or companies .\n"
+
+        output_file_obj = StringIO.StringIO()
+        GoldGenerator.correct_file(input_file_obj, output_file_obj)
+
+        self.assertEqual(output_file_obj.getvalue(), expected_output, output_file_obj.getvalue())
 
 if __name__ == '__main__':
     unittest.main()
