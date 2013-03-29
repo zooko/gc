aa7294cdc8847e9a2839540b0dd93f11f813e7dd
Amber on abhyasa <amber@abhyasa>
2013-03-29 17:17:35 -0600

    Removed old template tests from tmpipe.

--- code/language_modelling/test/test_tmpipe.py
+++ code/language_modelling/test/test_tmpipe.py
@@ -46,25 +46,6 @@ class TmpipeUnkfulTest(unittest.TestCase):
 
     def test_trigram_probability(self):
         self.skipTest("")
-        tmpipeobj.stdin.write('t , " says\n')
-        l = tmpipeobj.stdout.readline()
-        assert abs(-0.659269 - float(l)) < EPSILON, l
-        tmpipeobj.stdin.write("t it will be\n")
-        l = tmpipeobj.stdout.readline()
-        assert abs(-1.009832 - float(l)) < EPSILON, l
-        tmpipeobj.stdin.write("t it will not\n")
-        l = tmpipeobj.stdout.readline()
-        assert abs(-2.440987 - float(l)) < EPSILON, l
-
-    def test_unigram_backoff(self):
-        self.skipTest("")
-        tmpipeobj.stdin.write("o it\n")
-        l = tmpipeobj.stdout.readline()
-        assert abs(-0.260354 - float(l)) < EPSILON, l
-        tmpipeobj.stdin.write("o zero\n")
-        l = tmpipeobj.stdout.readline()
-        assert abs(-0.2765851 - float(l)) < EPSILON, l
-        
 
 if __name__ == '__main__':
     unittest.main()
