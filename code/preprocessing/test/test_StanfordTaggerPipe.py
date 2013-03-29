e4203c7d33ae6324edc8b9b7dfd62d940f66801b
Amber on abhyasa <amber@abhyasa>
2013-03-28 20:31:56 -0600

    Moved once-only setUp outside of the test class.

--- code/preprocessing/test/test_StanfordTaggerPipe.py
+++ code/preprocessing/test/test_StanfordTaggerPipe.py
@@ -4,56 +4,54 @@
 from code.preprocessing import StanfordTaggerPipe
 import unittest
 
+stanford_tagger_path = 'stanford-tagger/stanford-postagger.jar:'
+module_path = 'edu.stanford.nlp.tagger.maxent.MaxentTagger'
+model_path = 'stanford-tagger/english-bidirectional-distsim.tagger'
+
+tagger_pipe = StanfordTaggerPipe.StanfordTaggerPipe(stanford_tagger_path, module_path, model_path)
+
 class StanfordTaggerPipeTest(unittest.TestCase):
-
-    def setUp(self):
-
-        stanford_tagger_path = 'stanford-tagger/stanford-postagger.jar:'
-        module_path = 'edu.stanford.nlp.tagger.maxent.MaxentTagger'
-        model_path = 'stanford-tagger/english-bidirectional-distsim.tagger'
-        
-        self.tagger_pipe = StanfordTaggerPipe.StanfordTaggerPipe(stanford_tagger_path, module_path, model_path)
 
     def test_stanford_tagger_pipe(self):
 
-        self.tagger_pipe.stdin.write("Hello, World.\n")
-        result = self.tagger_pipe.stdout.readline()
+        tagger_pipe.stdin.write("Hello, World.\n")
+        result = tagger_pipe.stdout.readline()
         self.assertEqual(result, "Hello_UH ,_, World_NNP ._.\n",  repr(result))
-        result = self.tagger_pipe.stdout.readline()
+        result = tagger_pipe.stdout.readline()
         self.assertEqual(result, '\n', repr(result))
 
-        self.tagger_pipe.stdin.write("Hello again, World.\n")
-        result = self.tagger_pipe.stdout.readline()
+        tagger_pipe.stdin.write("Hello again, World.\n")
+        result = tagger_pipe.stdout.readline()
         self.assertEqual(result, "Hello_UH again_RB ,_, World_NNP ._.\n",  repr(result))
-        result = self.tagger_pipe.stdout.readline()
+        result = tagger_pipe.stdout.readline()
         self.assertEqual(result, '\n', repr(result))
 
-        self.tagger_pipe.stdin.write("Hello, World.\n")
-        result = self.tagger_pipe.stdout.readline()
+        tagger_pipe.stdin.write("Hello, World.\n")
+        result = tagger_pipe.stdout.readline()
         self.assertEqual(result, "Hello_UH ,_, World_NNP ._.\n",  repr(result))
-        result = self.tagger_pipe.stdout.readline()
+        result = tagger_pipe.stdout.readline()
         self.assertEqual(result, '\n', repr(result))
 
     def test_tags_list(self):
 
-        tags_list = self.tagger_pipe.tags_list("Hello, World.")
+        tags_list = tagger_pipe.tags_list("Hello, World.")
         self.assertEqual(tags_list, ["UH", ",", "NNP", "."], tags_list)
-        tags_list = self.tagger_pipe.tags_list("Hello again, World.")
+        tags_list = tagger_pipe.tags_list("Hello again, World.")
         self.assertEqual(tags_list, ["UH", "RB", ",", "NNP", "."], tags_list)
-        tags_list = self.tagger_pipe.tags_list("Hello, World.")
+        tags_list = tagger_pipe.tags_list("Hello, World.")
         self.assertEqual(tags_list, ["UH", ",", "NNP", "."], tags_list)
 
     def test_words_and_tags_list(self):
 
-        words_and_tags_list = self.tagger_pipe.words_and_tags_list("Hello, World.")
+        words_and_tags_list = tagger_pipe.words_and_tags_list("Hello, World.")
         self.assertEqual(words_and_tags_list, \
           [['Hello', 'UH'], [',', ','], ['World', 'NNP'], ['.', '.']], \
              words_and_tags_list)
-        words_and_tags_list = self.tagger_pipe.words_and_tags_list("Hello again, World.")
+        words_and_tags_list = tagger_pipe.words_and_tags_list("Hello again, World.")
         self.assertEqual(words_and_tags_list, \
           [['Hello', 'UH'], ['again', 'RB'], [',', ','], ['World', 'NNP'], ['.', '.']],  \
              words_and_tags_list)
-        words_and_tags_list = self.tagger_pipe.words_and_tags_list("Hello, World.")
+        words_and_tags_list = tagger_pipe.words_and_tags_list("Hello, World.")
         self.assertEqual(words_and_tags_list, \
           [['Hello', 'UH'], [',', ','], ['World', 'NNP'], ['.', '.']],  \
              words_and_tags_list)
