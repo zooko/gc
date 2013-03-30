f488bc0beb2ab97a83928fe3ad4825cc36b2a2f8
Amber on abhyasa <amber@abhyasa>
2013-03-30 08:53:56 -0600

    Added a setUp to test_StanfordTaggerPipe for readability.

--- code/preprocessing/test/test_StanfordTaggerPipe.py
+++ code/preprocessing/test/test_StanfordTaggerPipe.py
@@ -12,21 +12,27 @@ tagger_pipe = StanfordTaggerPipe.StanfordTaggerPipe(stanford_tagger_path, module
 
 class StanfordTaggerPipeTest(unittest.TestCase):
 
+    def setUp(self):
+
+        self.s0 = "Hello , World .\n"
+        self.s1 = "Hello again, World.\n"
+        self.s2 = "- { http : //uci.edu/features/2009/07/feature_alzheimersstemcell_090720.php } [ HYPERLINK : http : //uci.edu/features/2009/07/feature_alzheimersstemcell_090720.php ]\n"
+
     def test_stanford_tagger_pipe(self):
 
-        tagger_pipe.stdin_byte_writer.write('Hello, World.\n')
+        tagger_pipe.stdin_byte_writer.write(self.s0)
         result = tagger_pipe.stdout.readline()
         self.assertEqual(result, 'Hello_UH ,_, World_NNP ._.\n',  repr(result))
         result = tagger_pipe.stdout.readline()
         self.assertEqual(result, '\n', repr(result))
 
-        tagger_pipe.stdin_byte_writer.write('Hello again, World.\n')
+        tagger_pipe.stdin_byte_writer.write(self.s1)
         result = tagger_pipe.stdout.readline()
         self.assertEqual(result, 'Hello_UH again_RB ,_, World_NNP ._.\n',  repr(result))
         result = tagger_pipe.stdout.readline()
         self.assertEqual(result, '\n', repr(result))
 
-        tagger_pipe.stdin_byte_writer.write('- { http : //uci.edu/features/2009/07/feature_alzheimersstemcell_090720.php } [ HYPERLINK : http : //uci.edu/features/2009/07/feature_alzheimersstemcell_090720.php ]\n')
+        tagger_pipe.stdin_byte_writer.write(self.s2)
         result = tagger_pipe.stdout.readline()
         self.assertEqual(result, '-_: -LCB-_-LRB- http_NN :_: \/_: \/_: uci.edu\/features\/2009\/07\/feature_alzheimersstemcell_090720.php_NN -RCB-_-RRB- -LRB-_-LRB- HYPERLINK_NN :_: http_NN :_: \/_: \/_: uci.edu\/features\/2009\/07\/feature_alzheimersstemcell_090720.php_NN -RRB-_-RRB-\n',  repr(result))
         result = tagger_pipe.stdout.readline()
@@ -34,24 +40,24 @@ class StanfordTaggerPipeTest(unittest.TestCase):
 
     def test_tags_list(self):
 
-        tags_list = tagger_pipe.tags_list('Hello, World.')
+        tags_list = tagger_pipe.tags_list(self.s0)
         self.assertEqual(tags_list, ['_UH', '_,', '_NNP', '_.'], tags_list)
-        tags_list = tagger_pipe.tags_list('Hello again, World.')
+        tags_list = tagger_pipe.tags_list(self.s1)
         self.assertEqual(tags_list, ['_UH', '_RB', '_,', '_NNP', '_.'], tags_list)
-        tags_list = tagger_pipe.tags_list('- { http : //uci.edu/features/2009/07/feature_alzheimersstemcell_090720.php } [ HYPERLINK : http : //uci.edu/features/2009/07/feature_alzheimersstemcell_090720.php ]\n')
+        tags_list = tagger_pipe.tags_list(self.s2)
         self.assertListEqual(tags_list, ['_:', '_-LRB-', '_NN', '_:', '_:', '_:', '_NN', '_-RRB-', '_-LRB-', '_NN', '_:', '_NN', '_:', '_:', '_:', '_NN', '_-RRB-'],  tags_list)
 
     def test_words_and_tags_list(self):
 
-        words_and_tags_list = tagger_pipe.words_and_tags_list('Hello, World.')
+        words_and_tags_list = tagger_pipe.words_and_tags_list(self.s0)
         self.assertEqual(words_and_tags_list, \
           [('Hello', '_UH'), (',', '_,'), ('World', '_NNP'), ('.', '_.')], \
              words_and_tags_list)
-        words_and_tags_list = tagger_pipe.words_and_tags_list('Hello again, World.')
+        words_and_tags_list = tagger_pipe.words_and_tags_list(self.s1)
         self.assertEqual(words_and_tags_list, \
           [('Hello', '_UH'), ('again', '_RB'), (',', '_,'), ('World', '_NNP'), ('.', '_.')],  \
              words_and_tags_list)
-        words_and_tags_list = tagger_pipe.words_and_tags_list('- { http : //uci.edu/features/2009/07/feature_alzheimersstemcell_090720.php } [ HYPERLINK : http : //uci.edu/features/2009/07/feature_alzheimersstemcell_090720.php ]\n')
+        words_and_tags_list = tagger_pipe.words_and_tags_list(self.s2)
         self.assertListEqual(words_and_tags_list, \
              [('-', '_:'), ('-LCB-', '_-LRB-'), ('http', '_NN'), (':', '_:'), ('\\/', '_:'), ('\\/', '_:'), ('uci.edu\\/features\\/2009\\/07\\/feature_alzheimersstemcell_090720.php', '_NN'), ('-RCB-', '_-RRB-'), ('-LRB-', '_-LRB-'), ('HYPERLINK', '_NN'), (':', '_:'), ('http', '_NN'), (':', '_:'), ('\\/', '_:'), ('\\/', '_:'), ('uci.edu\\/features\\/2009\\/07\\/feature_alzheimersstemcell_090720.php', '_NN'), ('-RRB-', '_-RRB-')], \
              words_and_tags_list)
