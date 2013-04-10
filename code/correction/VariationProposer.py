a87177e1f2ebee38226e2784426511a9bed51d76
Amber on abhyasa <amber@abhyasa>
2013-04-09 20:51:21 -0600

    VariationProposer now has a function get alternatives which uses open or closed as necessary.

--- code/correction/VariationProposer.py
+++ code/correction/VariationProposer.py
@@ -3,15 +3,28 @@
 
 class VariationProposer():
 
-    def __init__(self, pos_tagger, tag_dictionary):
+    def __init__(self, pos_tagger, tag_dictionary, vocab_with_prefix):
         self.pos_tagger = pos_tagger
         self.tag_dictionary = tag_dictionary
+        self.vocab_with_prefix = vocab_with_prefix
 
 
-    def closed_class_alternatives(self, tokens):
-        last_tag = self.pos_tagger(tokens)[-1] 
-        if last_tag in ["IN", "DT"]:
-            ind = self.tag_dictionary[last_tag].index(tokens[-1])
-            return self.tag_dictionary[last_tag][:ind] + self.tag_dictionary[last_tag][ind+1:]
-        else:
-            return []
+    def closed_class_alternatives(self, token, tag):
+
+        ind = self.tag_dictionary[tag].index(token)
+        return self.tag_dictionary[tag][:ind] + self.tag_dictionary[tag][ind+1:]
+
+    def open_class_alternatives(self, token, tag_type):
+
+        prefix = token[:-4]
+        length = len(token)
+        keys = [k for k in self.tag_dictionary.keys() if k.startswith(tag_type)]
+        return [t for t in self.vocab_with_prefix(prefix) if len(t) <= length + 4 and reduce( lambda x,y: x or y, [t in self.tag_dictionary[k] for k in keys] ) ]
+
+    def get_alternatives(self, token, tag):
+
+        if tag in ["IN", "DT"]:
+            return self.closed_class_alternatives(token, tag)
+        if tag[:2] in ["NN", "VB"]:
+            return self.open_class_alternatives(token, tag[:2])
+        return []
--- code/correction/test/test_VariationProposer.py
+++ code/correction/test/test_VariationProposer.py
@@ -4,34 +4,42 @@
 from code.correction import VariationProposer
 import unittest
 
-tags = ["CC", "DT ", "IN ", "JJ ", "MD ", "NN ", "NNS", "VB ", "."]
-tag_dictionary = {"DT": ["a", "the", "any", "another", ""], "IN": ["with", "from", "of", ""], "CC": ["and", "but", "or"]}
+tag_dictionary = {"DT": ["a", "the", "any", "another", ""], "IN": ["with", "from", "of", ""], "CC": ["and", "but", "or"], "VB": ["like", "love"], "VBD": ['loved'], "VBG": ['loving']}
 
 def pos_tagger(tokens):
     return ['PRP', 'VBD', 'IN', 'DT', 'NN', 'WDT', 'VBD', 'JJR', 'IN', 'NN'][:len(tokens)]
-    
 
-proposer = VariationProposer.VariationProposer(pos_tagger, tag_dictionary)
+l_vars = ['laboured', 'labyrinths', 'laden', 'lamp', 'like', 'love', 'lover', 'loves', 'loving']
+
+def vocab_with_prefix(prefix):
+    if prefix == 'l':
+        return l_vars
+    if prefix == '':
+        return ['am', 'are', 'bad', 'cue', 'did', 'is']
+    return []
+
+proposer = VariationProposer.VariationProposer(pos_tagger, tag_dictionary, vocab_with_prefix)
 
 class VariationProposerTest(unittest.TestCase):
 
     def test_closed_class_alternatives(self):
-        
+
         tokens = "We loved with a love that was more than love .".lower().split()
+        tags = pos_tagger(tokens)
 
-        proposed = proposer.closed_class_alternatives(tokens[:1])
+        proposed = proposer.get_alternatives(tokens[0], tags[0])
         self.assertListEqual(proposed, [])
 
-        proposed = proposer.closed_class_alternatives(tokens[:2])
-        self.assertListEqual(proposed, [])
+        proposed = proposer.get_alternatives(tokens[1], tags[1])
+        self.assertListEqual(proposed, ['like', 'love', 'loving'])
 
-        proposed = proposer.closed_class_alternatives(tokens[:3])
+        proposed = proposer.get_alternatives(tokens[2], tags[2])
         self.assertListEqual(proposed, ["from", "of", ""], proposed)
 
-        proposed = proposer.closed_class_alternatives(tokens[:4])
+        proposed = proposer.get_alternatives(tokens[3], tags[3])
         self.assertListEqual(proposed, ["the", "any", "another", ""], proposed)
-    
-        
+
+
 
 
 
