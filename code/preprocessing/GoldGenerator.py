6a5835f6593a610e0d98e69545a850967564d326
Amber on abhyasa <amber@abhyasa>
2013-04-14 13:18:39 -0600

    During GoldGeneration is a good time to extract tokens that have been attested to be inserted or deleted.

--- code/preprocessing/GoldGenerator.py
+++ code/preprocessing/GoldGenerator.py
@@ -4,7 +4,9 @@
 # Takes the m2scorer formatted file and generates a file with one
 # corrected sentence per line.
 
-def correct_sentence(sentence, corrections):
+import json
+
+def correct_sentence_and_collect_insertables_and_deletables(sentence, corrections, insertables, deletables):
     tokens = sentence.split()[1:]
     span_substitions = []
     for line in corrections:
@@ -12,17 +14,32 @@ def correct_sentence(sentence, corrections):
         indices = [int(x) for x in correction_string_tokens[0].split()[1:]]
         substitution_string = correction_string_tokens[2]
         span_substitions.append((indices, substitution_string))
+
     span_substitions.sort()
     corrected = ""
     index = 0
     for sub in span_substitions:
         corrected += " ".join(tokens[index:sub[0][0]]) + " " + sub[1] + " "
         index = sub[0][1]
+        # Get insertables and deletables
+        sub_tokens = sub[1].split()
+        if len(sub_tokens) > sub[0][1] - sub[0][0]: # we have an inserted word
+            original_tokens = tokens[sub[0][0]:sub[0][1]]
+            for t in sub_tokens:
+                if t not in original_tokens:
+                    insertables.add(t.lower())
+        if len(sub_tokens) < sub[0][1] - sub[0][0]: # we have a deleted word
+            original_tokens = tokens[sub[0][0]:sub[0][1]]
+            for t in original_tokens:
+                if t not in sub_tokens:
+                    deletables.add(t.lower())
     corrected += " ".join(tokens[index:])
     return " ".join(corrected.split()) + '\n'
 
-def correct_file(m2_file_obj, gold_file_obj):
+def correct_file(m2_file_obj, gold_file_obj, ins_file_obj, del_file_obj):
     corrections = []
+    insertables = set([])
+    deletables = set([])
     new_sentence = True
     for line in m2_file_obj:
         if line.startswith("S "):
@@ -34,12 +51,17 @@ def correct_file(m2_file_obj, gold_file_obj):
             corrections.append(line)
         else:
             assert line == '\n', line
-            gold_file_obj.write(correct_sentence(sentence, corrections))
+            gold_file_obj.write(correct_sentence_and_collect_insertables_and_deletables(sentence, corrections, insertables, deletables))
             corrections = []
             new_sentence = True
+
+    json.dump(list(insertables), ins_file_obj)
+    json.dump(list(deletables), del_file_obj)
 
 if __name__ == '__main__':
     import sys
     m2_file_obj = sys.stdin
     gold_file_obj = sys.stdout
-    correct_file(m2_file_obj, gold_file_obj)
+    ins_file_obj = open(sys.argv[1], 'w')
+    del_file_obj = open(sys.argv[2], 'w')
+    correct_file(m2_file_obj, gold_file_obj, ins_file_obj, del_file_obj)
--- code/preprocessing/test/test_GoldGenerator.py
+++ code/preprocessing/test/test_GoldGenerator.py
@@ -2,31 +2,54 @@
 # test_GoldGenerator.py
 
 from code.preprocessing import GoldGenerator
-import unittest, StringIO
+import unittest, StringIO, json
 
 class GoldTest(unittest.TestCase):
 
-    def test_correct_sentence(self):
+    def test_correct_sentence_and_collect_insertables_and_deletables(self):
 
         sentence = "S Our current population is 6 billion people and it is still growing exponentially .\n"
         corrections = []
-        corrected = GoldGenerator.correct_sentence(sentence, corrections)
+        insertables = set([])
+        deletables = set([])
+        corrected = GoldGenerator.correct_sentence_and_collect_insertables_and_deletables(sentence, corrections, insertables, deletables)
         self.assertEqual(corrected, "Our current population is 6 billion people and it is still growing exponentially .\n", corrected)
+        self.assertSetEqual(insertables, set([]), insertables)
+        self.assertSetEqual(deletables, set([]), deletables)
 
         sentence = "S This will , if not already , caused problems as there are very limited spaces for us .\n"
+        insertables = set([])
+        deletables = set([])
         corrections = ["A 7 8|||Vform|||cause|||REQUIRED|||-NONE-|||0", 
                        "A 14 15|||Nn|||space|||REQUIRED|||-NONE-|||0",
                        "A 11 12|||SVA|||is|||REQUIRED|||-NONE-|||0"]
-        corrected = GoldGenerator.correct_sentence(sentence, corrections)
+        corrected = GoldGenerator.correct_sentence_and_collect_insertables_and_deletables(sentence, corrections, insertables, deletables)
         self.assertEqual(corrected, "This will , if not already , cause problems as there is very limited space for us .\n", corrected)
-
+        self.assertSetEqual(insertables, set([]), insertables)
+        self.assertSetEqual(deletables, set([]), deletables)
 
         sentence = "S It is also important to create a better material that can support the buildings despite any natural disaster like earthquakes .\n"
+        insertables = set([])
+        deletables = set([])
         corrections = ["A 6 7|||ArtOrDet||||||REQUIRED|||-NONE-|||0",
                        "A 12 13|||ArtOrDet||||||REQUIRED|||-NONE-|||0",
                        "A 17 18|||Nn|||disasters|||REQUIRED|||-NONE-|||0"]
-        corrected = GoldGenerator.correct_sentence(sentence, corrections)
+        corrected = GoldGenerator.correct_sentence_and_collect_insertables_and_deletables(sentence, corrections, insertables, deletables)
         self.assertEqual(corrected, "It is also important to create better material that can support buildings despite any natural disasters like earthquakes .\n", corrected)
+        self.assertSetEqual(insertables, set([]), insertables)
+        self.assertSetEqual(deletables, set(['the', 'a']), deletables)
+
+        sentence = "S In this era , Engineering designs can help to provide more habitable accommodation by designing a stronger material so it 's possible to create a taller and safer building , a better and efficient sanitation system to prevent disease , and also by designing a way to change the condition of the inhabitable environment ."
+        insertables = set([])
+        deletables = set([])
+        corrections = ["A 15 16|||ArtOrDet||||||REQUIRED|||-NONE-|||0",
+                       "A 24 29|||ArtOrDet|||taller and safer buildings|||REQUIRED|||-NONE-|||0"]
+        corrected = GoldGenerator.correct_sentence_and_collect_insertables_and_deletables(sentence, corrections, insertables, deletables)
+        self.assertEqual(corrected, "In this era , Engineering designs can help to provide more habitable accommodation by designing stronger material so it 's possible to create taller and safer buildings , a better and efficient sanitation system to prevent disease , and also by designing a way to change the condition of the inhabitable environment .\n", corrected)
+        self.assertSetEqual(insertables, set([]), insertables)
+        self.assertSetEqual(deletables, set(['a', 'building']), deletables)
+        
+
 
     def test_correct_file(self):
 
@@ -63,9 +86,14 @@ class GoldTest(unittest.TestCase):
         expected_output = "However , when the design had been carried out for a few weeks , it was stopped .\nThe government 's explanation was because of safety consideration for earthquakes and some other disasters .\nBut such an explanation was non-sense as the engineers did not see such a possibility during their designing ; otherwise the design would not be carried out at all .\nSome evidences showed that the reason for the halt of the work was that the higher level authorities thought an underground traffic system was not necessary for Harbin .\nIn this example , there is nothing wrong with the original design work , but the design still can not be carried out due to political inconsistent between the city government and higher level authorities .\nThe similarities of political obstacles in both two steps are that such problems can not be easily solved by the engineering group , in other words , the political problems are unavoidable to some degree .\nAnother problem , financial issue , is quite common during the design process .\nIt affects the analyzing and the testing step most seriously though it can affect almost every step of design work .\nFor most design work , engineers are always employed by some organizations or companies .\n"
 
         output_file_obj = StringIO.StringIO()
-        GoldGenerator.correct_file(input_file_obj, output_file_obj)
+        ins_file_obj = StringIO.StringIO()
+        del_file_obj = StringIO.StringIO()
+        GoldGenerator.correct_file(input_file_obj, output_file_obj, ins_file_obj, del_file_obj)
 
         self.assertEqual(output_file_obj.getvalue(), expected_output, output_file_obj.getvalue())
+
+        self.assertSetEqual(set(json.loads(ins_file_obj.getvalue())), set(['a', 'the', 'testing', 'an']))
+        self.assertSetEqual(set(json.loads(del_file_obj.getvalue())), set(['of', 'the']))
 
 if __name__ == '__main__':
     unittest.main()
