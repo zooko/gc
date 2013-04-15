4d3ae43819b73c133f92ee0bf2852416970ab206
Amber on abhyasa <amber@abhyasa>
2013-04-14 19:32:35 -0600

    Incomplete, but committing for safety: experimenting with corrector.

--- code/correction/Corrector.py
+++ code/correction/Corrector.py
@@ -1,25 +1,63 @@
 # L. Amber Wilcox-O'Hearn 2013
 # Corrector.py
 
-def beam_search(sentence, width, prob_of_err_func, path_prob_func, variation_generator):
+
+def beam_search(tokens, width, prob_of_err_func, path_prob_func, variation_generator):
     """
-    For each token in the sentence, put all variations on every
+    For each token in the tokens, put all variations on every
     existing path on the beam.  Sort and prune. At the end, return the
     best path.
     A path is just a tuple of a log probability and a list of tokens.
     """
 
     beam = [(0, [])]
-    for i in range(len(sentence)):
+    for i in range(len(tokens)):
         new_beam = []
         for path in beam:
+            print "I'm a path in the beam:", path
             path_log_prob, path_tokens = path
-            path_with_next_original_token = path_tokens + [sentence[i]]
-            new_beam.append( (path_log_prob + path_prob_func(path_with_next_original_token), path_with_next_original_token) )
-            for path_variation in variation_generator(path_with_next_original_token):
-                new_beam.append( (path_log_prob + path_prob_func(path_variation) + prob_of_err_func(path_variation), path_variation) )
+            path_with_next_original_token = path_tokens + [tokens[i]]
+            print "Here I am with next token:", path_with_next_original_token
+            prob = path_prob_func(path_with_next_original_token)
+            print "Here's my prob:", prob
+            new_beam.append( (path_log_prob + prob, path_with_next_original_token) )
+            token_string = ' '.join(path_with_next_original_token)
+            for path_variation in variation_generator(token_string):
+                print "I'm a variation:", path_variation
+                prob = path_prob_func(path_variation)
+                print "And here's my prob:", prob
+                new_beam.append( (path_log_prob + prob + prob_of_err_func(path_variation), path_variation) )
 
         new_beam.sort()
         beam = new_beam[-width:]
+        print "I am a beam:", beam, '\n'
 
     return beam[-1][1]
+
+class Corrector():
+
+    def __init__(self, trigram_model_pipe, width, variation_generator, error_prob):
+
+        self.trigram_model_pipe = trigram_model_pipe
+        self.width = width
+        self.variation_generator = variation_generator
+        self.error_prob = error_prob
+
+    def get_error_prob(self, tokens):
+       return self.error_prob
+
+    def trigram_path_probability(self, path):
+       '''
+       path is a list of tokens with at least one element.
+       '''
+       if len(path) == 1:
+          return self.trigram_model_pipe.trigram_probability(['<s>', '<s>', path[0]])
+       if len(path) == 2:
+          return self.trigram_model_pipe.trigram_probability(['<s>', path[0], path[1]])
+       else:
+          return self.trigram_model_pipe.trigram_probability(path[-3:])
+
+
+    def get_correction(self, tokens):
+       
+        return beam_search(tokens, self.width, self.get_error_prob, self.trigram_path_probability, self.variation_generator)
--- code/correction/test/test_Corrector.py
+++ code/correction/test/test_Corrector.py
@@ -1,12 +1,14 @@
 # L. Amber Wilcox-O'Hearn 2013
 # test_Corrector.py
 
-from code.correction import Corrector
-import unittest
+from code.correction import Corrector, VariationProposer
+from code.preprocessing import StanfordTaggerPipe
+import unittest, json
 
 def probability_of_error_function(tokens):
     return 0.01
-def variation_generator(tokens):
+def variation_generator(sentence):
+    tokens = sentence.split()
     token = tokens[-1]
     if len(token) > 2:
         return []
@@ -22,20 +24,36 @@ class CorrectorTest(unittest.TestCase):
 
     def test_beam_search(self):
 
-        sentence = ['This', 'is', 'a', 'bad', 'sentence', '.']
+        tokens = ['This', 'is', 'a', 'bad', 'sentence', '.']
 
         width = 5
-        best_sentence = Corrector.beam_search(sentence, width, probability_of_error_function, path_probability_function, variation_generator)
-        self.assertListEqual(best_sentence, ['This', 'isa', 'aa', 'bad', 'sentence', '.a'])
+        best_tokens = Corrector.beam_search(tokens, width, probability_of_error_function, path_probability_function, variation_generator)
+        self.assertListEqual(best_tokens, ['This', 'isa', 'aa', 'bad', 'sentence', '.a'])
 
         width = 50
-        best_sentence = Corrector.beam_search(sentence, width, probability_of_error_function, path_probability_function, variation_generator)
-        self.assertListEqual(best_sentence, ['This', 'isd', 'aa', 'bad', 'sentence', '.a'])
+        best_tokens = Corrector.beam_search(tokens, width, probability_of_error_function, path_probability_function, variation_generator)
+        self.assertListEqual(best_tokens, ['This', 'isd', 'aa', 'bad', 'sentence', '.a'])
 
-        sentence = ['This', 'isn\'t', 'bad', '...']
-        best_sentence = Corrector.beam_search(sentence, width, probability_of_error_function, path_probability_function, variation_generator)
-        self.assertListEqual(best_sentence, ['This', 'isn\'t', 'bad', '...'])
+        tokens = ['This', 'isn\'t', 'bad', '...']
+        best_tokens = Corrector.beam_search(tokens, width, probability_of_error_function, path_probability_function, variation_generator)
+        self.assertListEqual(best_tokens, ['This', 'isn\'t', 'bad', '...'])
 
+    def test_get_correction(self):
+
+        from BackOffTrigramModel import BackOffTrigramModelPipe
+        tmpipe_obj = BackOffTrigramModelPipe.BackOffTMPipe('BackOffTrigramModelPipe', 'code/correction/test/trigram_model_5K.arpa')
+        stanford_tagger_path = 'stanford-tagger/stanford-postagger.jar:'
+        module_path = 'edu.stanford.nlp.tagger.maxent.MaxentTagger'
+        model_path = 'stanford-tagger/english-left3words-distsim.tagger'
+        tagger_pipe = StanfordTaggerPipe.StanfordTaggerPipe(stanford_tagger_path, module_path, model_path)
+        pos_dictionary = json.load(open('code/correction/test/pos_dictionary', 'r'))
+        small_insertables = json.load(open('code/correction/test/small_insertables', 'r'))
+        var_gen = VariationProposer.VariationProposer(tagger_pipe.tags_list, pos_dictionary, tmpipe_obj.vocabulary_with_prefix, small_insertables)
+        corrector = Corrector.Corrector(tmpipe_obj, 10, var_gen.generate_path_variations, -3.3)
+
+        tokens = "This will , if not already , caused problems as there are very limited spaces for us .".lower().split()
+        result = corrector.get_correction(tokens)
+        self.assertEqual(result, '', result)
 
 if __name__ == '__main__':
     unittest.main()
